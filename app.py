c8cb0bb42a05b33cfc9a455b2a549fd3c8aedf39
Emanuelly Nicolly Garcia Santos <321952244+emanuellyngs-hub@users.noreply.github.com>
2026-09-13 13:04:06 -0300

    Segunda versão do python

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ else:
 # calcula o valor do desconto:
 valor_desconto = valor_compra * percentual_desconto / 100
 
-# calcula o valor final a pagar:
+# calcula o valor final da compras:
 valor_final = valor_compra - valor_desconto
 
 #SAÍDA
 # exibe os resultados para o usuário
 print("\n--- RESULTADO DA COMPRA---")
 print(f"Valor da compra: R$ {valor_compra:.2f}")
-print(f"dESconto aplicado: {percentual_desconto}%")
+print(f"Desconto aplicado: {percentual_desconto}%")
 print(f"Valor do desconto: R$ {valor_desconto:.2f}")
 print(f"Valor total a pagar: R$ {valor_final:.2f}")
